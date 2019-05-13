476fea6a96500110f075138a9e6bdfa3b47780a5
YouJin <ysh2491@naver.com>
2019-05-14 06:53:21 +0900

    commit for deploy

--- ewhafest2019/settings.py
+++ ewhafest2019/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')^emf@_c=*7v=d2qyc!!goj3z8#q1hyj=_kskk$l+zb2bbw)1='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')^emf@_c=*7v=d2qyc!!goj3z8#q1hyj=_kskk$l+zb2bbw)1=')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
